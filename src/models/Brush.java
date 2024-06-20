e34b7cfb0f84db1ca198995c210933528a8a8410
David Yousry <davidyousry45@gmail.com>
2024-06-20 03:17:25 +0300

    Brush model added

--- src/models/Brush.java
+++ src/models/Brush.java
@@ -1,5 +1,43 @@
 package PaintRemake.src.models;
 
-public class Brush  implements Drawing{
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Brush implements Drawing{
+    private ArrayList<Point> points = new ArrayList<Point>();
+    private Color color;
+
+
+
+    public Brush(Color color){
+        this.color = color;
+    }
+
+
+    public void addPoint(int x1, int y1){
+        points.add(new Point(x1,y1));
+    }
+
+    public ArrayList<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(ArrayList<Point> points) {
+        this.points = points;
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(Color color) {
+        this.color = color;
+        
+    }
+
+    
 
 }
